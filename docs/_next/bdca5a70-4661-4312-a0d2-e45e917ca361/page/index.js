c20ca20140b06bd08743a83b0fa9b1e8eb9bdcb5
jaketrent <trent.jake@gmail.com>
2017-07-13 07:54:11 -0600

    chore: regen site

--- docs/_next/bdca5a70-4661-4312-a0d2-e45e917ca361/page/index.js
+++ docs/_next/bdca5a70-4661-4312-a0d2-e45e917ca361/page/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([2],[],[344]);
+            var comp = module.exports=webpackJsonp([2],[],[349]);
             return { page: comp.default }
           })
         
